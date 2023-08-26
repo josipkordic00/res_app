563458cce3af84dfc9f2f650128907cd653d863e
josipkordic00 <josipkordic00@gmail.com>
2023-08-26 18:08:26 +0200

    finall

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://calm-forest-35172-38d7ff5b8b11.herokuapp.com/api/', // Set the base URL for your API
+  baseURL: 'http://localhost:8000/api', // Set the base URL for your API
   // You can also configure other Axios options here
 });
 
